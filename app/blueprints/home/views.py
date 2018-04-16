253037087faded6b51e3bb9b8d891afba9160948
jobo8505 <boegeholz.joern@gmail.com>
2018-04-16 17:53:56 +0200

    add more endpoints

--- app/blueprints/home/views.py
+++ app/blueprints/home/views.py
@@ -5,3 +5,12 @@ user_stats = Blueprint('user_stats', __name__, template_folder='templates')
 @user_stats.route('/')
 def hello_world():
     return 'Hello World!'
+
+def index():
+    print("hello index!")
+
+
+def get_users():
+    print("hello users!")
+
+#TODO
