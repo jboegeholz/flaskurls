a95d07b153df434ad00a0046f91cefb7996f0e97
jobo8505 <boegeholz.joern@gmail.com>
2018-04-17 10:00:48 +0200

    add meta files

---
+++ README.md
@@ -0,0 +1,5 @@
+# flaskurls
+Django-style url handling for Flask
+
+#Travis-CI
+[![Build Status](https://travis-ci.org/jboegeholz/flaskurls.svg?branch=master)](https://travis-ci.org/jboegeholz/flaskurls)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='flask_urls',
+    version='0.1',
+    packages=['flask_urls'],
+    url='https://github.com/jboegeholz/flaskurls',
+    download_url='https://github.com/jboegeholz/flaskurls/archive/0.1.tar.gz',
+    license='MIT',
+    author='Joern Boegeholz',
+    author_email='boegeholz.joern@gmail.com',
+    description='Django-style url handling for Flask'
+)
