914d6356126fc35cba888052f6b7016ab9666f17
jobo8505 <boegeholz.joern@gmail.com>
2018-05-11 17:16:03 +0200

    wrap function in class

--- flask_url_mapping/flask_urls.py
+++ flask_url_mapping/flask_urls.py
@@ -1,77 +1,95 @@
 import importlib.util  # needs backport to Python 2.7
 # https://stackoverflow.com/questions/45350363/backport-of-importlib-for-python-2-7-from-3-6
 import os
-
+from flask import current_app
 ERROR_MSG = """
 Wrong mapping format!
 The syntax is either (<route>, <function>, <http_method>) or (<prefix>, <module>)
 """
 
-_permissions = {}
 
+class FlaskUrls(object):
 
-def register_urls(app, urls, prefix=""):
-    app.logger.info("Registering Urls")
-    _register_urls(app, urls, prefix)
-    app.logger.info(app.url_map)
+    def __init__(self, app=None):
+        self._permissions = {}
+        if app is not None:
+            self.app = app
+            self.init_app(app)
 
+    def init_app(self, app):
+        app.url_value_preprocessors.setdefault(None, []).append(self._check_permissions)
 
-def _register_urls(app, urls, prefix):
-    """Main entry point of the module. Takes an array of url mappings and registers them as url_rule at the Flask app
-    :param app: a Flask app object
-    :param urls: an array of tuples of (<route>, <function>, <http_method>) or (<prefix>, <module>)
-    :param prefix: a prefix to the route
-    :return:
-    """
-    app.logger.info("_register_urls")
-    for url in urls:
-        number_of_args = len(url)
+    def _check_permissions(self, endpoint, values):
+        """
+        this function checks if a current user has the permission to 'use' the endpoint
+        :param endpoint:
+        :param values: not used but part of the signature
+        :return:
+        """
+        with self.app.app_context():
+            print(current_app.name)
+            print("_check_permissions for: " + endpoint)
 
-        if number_of_args == 2 and isinstance(url[0], str) and hasattr(url[1], '__call__'):
-            _register_endpoint(app, prefix, url[0], url[1], ["GET"])
-        elif number_of_args == 2 and isinstance(url[0], str) and isinstance(url[1], str):
-            _register_component(app, url)
-        elif number_of_args == 3 and isinstance(url[0], str) and hasattr(url[1], '__call__') and isinstance(url[2], list):
-            _register_endpoint(app, prefix, url[0], url[1], url[2])
-        elif number_of_args == 4 and isinstance(url[0], str) and hasattr(url[1], '__call__') \
-                and isinstance(url[2], list) and isinstance(url[3], str):
-            _register_endpoint(app, prefix, url[0], url[1], url[2])
-            _permissions[os.path.join(prefix, url[0])] = url[3]
-            print(_permissions)
-        else:
-            raise Exception(ERROR_MSG)
+        pass
 
+    def register_urls(self, urls, prefix=""):
 
-def _register_component(app, url):
-    app.logger.info("Registering component: " + str(url))
-    prefix = url[0]
-    urls_file = url[1]
-    app.logger.info("Importing: " + urls_file)
-    urls_file = urls_file.split(".")
-    package = urls_file[0]
-    module_name = urls_file[1]
-    module_path = os.path.join(app.root_path, package)
-    urls_path = os.path.join(module_path, module_name + ".py")
-    loaded_module = _load_module(urls_path)
-    _register_urls(app, loaded_module.urls, prefix)
-    _append_template_path(app, module_path)
+        self.app.logger.info("Registering Urls")
+        self._register_urls(urls, prefix)
+        self.app.logger.info(self.app.url_map)
 
+    def _register_urls(self, urls, prefix):
+        """Main entry point of the module. Takes an array of url mappings and registers them as url_rule at the Flask app
+        :param app: a Flask app object
+        :param urls: an array of tuples of (<route>, <function>, <http_method>) or (<prefix>, <module>)
+        :param prefix: a prefix to the route
+        :return:
+        """
+        self.app.logger.info("_register_urls")
+        for url in urls:
+            number_of_args = len(url)
 
-def _append_template_path(app, module_path):
-    template_path = os.path.join(module_path, "templates")
-    if os.path.isdir(template_path):
-        app.jinja_loader.searchpath.append(template_path)
+            if number_of_args == 2 and isinstance(url[0], str) and hasattr(url[1], '__call__'):
+                self._register_endpoint(prefix, url[0], url[1], ["GET"])
+            elif number_of_args == 2 and isinstance(url[0], str) and isinstance(url[1], str):
+                self._register_component(url)
+            elif number_of_args == 3 and isinstance(url[0], str) and hasattr(url[1], '__call__') and isinstance(url[2], list):
+                self._register_endpoint(prefix, url[0], url[1], url[2])
+            elif number_of_args == 4 and isinstance(url[0], str) and hasattr(url[1], '__call__') \
+                    and isinstance(url[2], list) and isinstance(url[3], str):
+                self._register_endpoint(prefix, url[0], url[1], url[2])
+                self._permissions[os.path.join(prefix, url[0])] = url[3]
+                print(self._permissions)
+            else:
+                raise Exception(ERROR_MSG)
 
+    def _register_component(self, url):
+        self.app.logger.info("Registering component: " + str(url))
+        prefix = url[0]
+        urls_file = url[1]
+        self.app.logger.info("Importing: " + urls_file)
+        urls_file = urls_file.split(".")
+        package = urls_file[0]
+        module_name = urls_file[1]
+        module_path = os.path.join(self.app.root_path, package)
+        urls_path = os.path.join(module_path, module_name + ".py")
+        loaded_module = self._load_module(urls_path)
+        self._register_urls(loaded_module.urls, prefix)
+        self._append_template_path(module_path)
 
-def _load_module(module_path):
-    spec = importlib.util.spec_from_file_location("urls", module_path)
-    foo = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(foo)
-    return foo
+    def _append_template_path(self, module_path):
+        template_path = os.path.join(module_path, "templates")
+        if os.path.isdir(template_path):
+            self.app.jinja_loader.searchpath.append(template_path)
 
+    def _load_module(self, module_path):
+        spec = importlib.util.spec_from_file_location("urls", module_path)
+        foo = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(foo)
+        return foo
 
-def _register_endpoint(app, prefix, route, view_func, methods):
-    app.logger.info("Registering endpoint: " + str(route))
-    if prefix:
-        route = prefix + route
-    app.add_url_rule(route, methods=methods, view_func=view_func)
+    def _register_endpoint(self, prefix, route, view_func, methods):
+        self.app.logger.info("Registering endpoint: " + str(route))
+        if prefix:
+            route = prefix + route
+        self.app.add_url_rule(route, methods=methods, view_func=view_func)
--- test/test_flask_urls.py
+++ test/test_flask_urls.py
@@ -12,78 +12,86 @@ class FlaskUrlsTest(unittest.TestCase):
         pass
 
     def test_root(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         from test.testapp import views
         urls = [
             ("/", views.index, ["GET"]),
         ]
-        register_urls(self.test_app.application, urls)
+        flask_urls = FlaskUrls(self.test_app.application)
+        flask_urls.register_urls(urls)
         result = self.test_app.get('/')
         self.assertEqual(result.status_code, 200)
 
     def test_users(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         from test.testapp import views
         urls = [
             ("/users", views.get_users, ["GET"]),
         ]
-        register_urls(self.test_app.application, urls)
+        flask_urls = FlaskUrls(self.test_app.application)
+        flask_urls.register_urls(urls)
         result = self.test_app.get('/users')
         self.assertEqual(result.status_code, 200)
 
     def test_home_index(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         urls = [
             ("/home", "home.urls")
         ]
-        register_urls(self.test_app.application, urls)
+        flask_urls = FlaskUrls(self.test_app.application)
+        flask_urls.register_urls(urls)
         result = self.test_app.get('/home/home_index')
         self.assertEqual(result.status_code, 200)
 
     def test_home_users(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         urls = [
             ("/home", "home.urls")
         ]
-        register_urls(self.test_app.application, urls)
+        flask_urls = FlaskUrls(self.test_app.application)
+        flask_urls.register_urls(urls)
         result = self.test_app.get('/home/home_users')
         self.assertEqual(result.status_code, 200)
 
     def test_missing_http_method(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         from test.testapp import views
         urls = [
             ("/", views.index),
         ]
-        register_urls(self.test_app.application, urls)
+        flask_urls = FlaskUrls(self.test_app.application)
+        flask_urls.register_urls(urls)
         result = self.test_app.get('/')
         self.assertEqual(result.status_code, 200)
 
     def test_wrong_mapping_format_no_endpoint(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         urls = [
             "/"
         ]
         with self.assertRaises(Exception) as context:
-            register_urls(self.test_app.application, urls)
+            flask_urls = FlaskUrls(self.test_app.application)
+            flask_urls.register_urls(urls)
         self.assertTrue('Wrong mapping format!' in str(context.exception))
 
     def test_templates(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         urls = [
             ("/home", "home.urls")
         ]
-        register_urls(self.test_app.application, urls)
+        flask_urls = FlaskUrls(self.test_app.application)
+        flask_urls.register_urls(urls)
 
         result = self.test_app.get('/home/home_html')
         self.assertEqual(result.status_code, 200)
 
     def test_require_roles(self):
-        from flask_url_mapping import register_urls
+        from flask_url_mapping import FlaskUrls
         from test.testapp import views
         urls = [
             ("/admin", views.index, ["GET"], "admin"),
         ]
-        register_urls(self.test_app.application, urls)
+        flask_urls = FlaskUrls(self.test_app.application)
+        flask_urls.register_urls(urls)
         result = self.test_app.get('/admin')
         self.assertEqual(result.status_code, 200)
