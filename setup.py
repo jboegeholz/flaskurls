90068b79287342317093a873b02effa233f2a2af
jobo8505 <boegeholz.joern@gmail.com>
2018-07-24 16:48:33 +0200

    add Pipfile update version to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='flask_url_mapping',
-    version='0.3',
+    version='0.4',
     packages=['flask_url_mapping'],
     url='https://github.com/jboegeholz/flaskurls',
     download_url='https://github.com/jboegeholz/flaskurls/archive/0.2.tar.gz',
